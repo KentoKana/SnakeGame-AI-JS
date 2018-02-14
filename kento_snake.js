4a4ae2e97b53ee7a3c70347ff61d4790039ee14b
Kento K <33736354+KentoKana@users.noreply.github.com>
2018-02-14 12:18:16 -0800

    Update kento_snake.js
    Added function to find snake head coordinate and apple coordinate.

--- kento_snake.js
+++ kento_snake.js
@@ -5,6 +5,14 @@ const width = height;
 //All the states that a cell can hold.
 const cellState = {snakeHead: 'S', snakeBody: '0', apple: 'A', cell: '_'  }
 
+var gridObject;
+var gameState;
+
+var findSnake;
+var findApple;
+
+
+
 
 //Creates an array of objects for the grid. Object will hold the x and y value, as well as Boolean values for snake and apple.
 function makeGridObject() {
@@ -12,13 +20,13 @@ function makeGridObject() {
 	for(i=0;i<width;i++){
 		grid[i] = []
 		for(j=0;j<width;j++){
-			grid[i][j] = {x: i, y: j, snake: false, apple: false}
+			grid[i][j] = {x: j, y: i, snake: false, apple: false}
 		}	
 	}
 	return grid;
 }
 
-var gridObject = makeGridObject()
+gridObject = makeGridObject()
 
 //Creates game grid. 
 function makeGrid(someGrid){
@@ -38,8 +46,41 @@ function makeGrid(someGrid){
 	return array;
 }
 
-var gameState = makeGrid(gridObject);
+gridObject[3][5].apple = true;
+gridObject[3][4].snake = true;
 
-console.log(gameState);
+gameState = makeGrid(gridObject);
 
+document.write('<br>' + '<br>' + gameState.join('<br>'))
+
+//Returns snake coordinate in the form [x, y]
+function lookForSnake(grid){
+	for(i=0;i<width;i++){
+		for(j=0;j<width;j++){
+			if(grid[i][j].snake === true){
+				var snekHeadCoordX = (grid[i][j].x);
+				var snekHeadCoordY= (grid[i][j].y);
+			}
+		}
+	}
+	var snakeCoord = [snekHeadCoordX, snekHeadCoordY];
+	return snakeCoord;
+}
+
+findSnake = lookForSnake(gridObject);
+
+function lookForApple(grid){
+	for(i=0;i<width;i++){
+		for(j=0;j<width;j++){
+			if(grid[i][j].apple === true){
+				var appleCoordX = (grid[i][j].x);
+				var appleCoordY = (grid[i][j].y);
+			}
+		}
+	}
+	var appleCoord = [appleCoordX, appleCoordY];
+	return appleCoord;
+}
+
+findApple = lookForApple(gridObject);
 
